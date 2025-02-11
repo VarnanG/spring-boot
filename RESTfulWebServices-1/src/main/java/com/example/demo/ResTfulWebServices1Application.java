f72f1323f62ef1af2b8012bc231c02fb02dc5771
Varnan Gupta <varnangupta113@gmail.com>
2025-02-11 14:17:54 +0530

    Day-8

---
+++ RESTfulWebServices-1/src/main/java/com/example/demo/ResTfulWebServices1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulWebServices1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulWebServices1Application.class, args);
+	}
+
+}
